6d5352dc77cf8b8474c752f1e96f75a893f5461c
Chuy <chuymaster3@hotmail.com>
2021-09-01 10:11:22 +0900

    Create animation_samples project

---
+++ animation_samples/lib/hero_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HeroPage extends StatelessWidget {
+  const HeroPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
