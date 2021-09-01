c60f6d87c49330de1b74ceec62c22dfdacf1e477
Chuy <chuymaster3@hotmail.com>
2021-09-01 09:44:54 +0900

    Add maxWidth constraint

--- layout_app/lib/responsive_design_page.dart
+++ layout_app/lib/responsive_design_page.dart
@@ -20,59 +20,62 @@ class _Body extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     return Center(
-        child: Column(
-      children: [
-        const _Spacer(),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 50),
-          child: Text(
-            "Flutter is Google's UI toolkit for building beautiful, natively compiled",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                height: 1.85,
-                fontWeight: FontWeight.bold),
-          ),
-        ),
-        const _Spacer(),
-        FlutterLogo(
-          size: height * 0.3,
-        ),
-        const _Spacer(),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 50),
-          child: Text(
-              'Fast Development Paint your app to life in milliseconds with Stateful Hot Reload. Use a rich set of ',
+        child: ConstrainedBox(
+      constraints: BoxConstraints(maxWidth: 650),
+      child: Column(
+        children: [
+          const _Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              "Flutter is Google's UI toolkit for building beautiful, natively compiled",
               textAlign: TextAlign.center,
               style: TextStyle(
                   color: Colors.white,
-                  fontSize: 15,
+                  fontSize: 16,
                   height: 1.85,
-                  fontWeight: FontWeight.bold)),
-        ),
-        const _Spacer(),
-        SizedBox(
-            height: 42,
-            width: 300,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  textStyle: TextStyle(color: Colors.white),
-                  primary: Theme.of(context).accentColor,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4))),
-              onPressed: () {},
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                child: Text(
-                  'get started',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const _Spacer(),
+          FlutterLogo(
+            size: height * 0.3,
+          ),
+          const _Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+                'Fast Development Paint your app to life in milliseconds with Stateful Hot Reload. Use a rich set of ',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    height: 1.85,
+                    fontWeight: FontWeight.bold)),
+          ),
+          const _Spacer(),
+          SizedBox(
+              height: 42,
+              width: 300,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    textStyle: TextStyle(color: Colors.white),
+                    primary: Theme.of(context).accentColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4))),
+                onPressed: () {},
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text(
+                    'get started',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-            )),
-        const _Spacer()
-      ],
+              )),
+          const _Spacer()
+        ],
+      ),
     ));
   }
 }
