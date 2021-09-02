e099511dcb3766986f0a862b18638010f76e69e3
Chuy <chuymaster3@hotmail.com>
2021-09-02 09:37:33 +0900

    Add mixed slide and rotation transition

--- animation_samples/lib/transition_page.dart
+++ animation_samples/lib/transition_page.dart
@@ -91,6 +91,38 @@ class TransitionPage extends StatelessWidget {
                 child: Text('Tweenのendが整数じゃないとダメな回転'),
               ),
             ),
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: 120,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.grey[300], onPrimary: Colors.black),
+                onPressed: () {
+                  Navigator.of(context).push(PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) {
+                    return _PageA();
+                  }, transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                    return SlideTransition(
+                      position: Tween(
+                              begin: const Offset(-1, -1),
+                              end: const Offset(0, 0))
+                          .animate(CurvedAnimation(
+                              parent: animation, curve: Curves.easeOutCubic)),
+                      child: RotationTransition(
+                        turns: Tween(begin: 0.0, end: 2.0).animate(
+                            CurvedAnimation(
+                                parent: animation, curve: Curves.easeOutCubic)),
+                        child: child,
+                      ),
+                    );
+                  }));
+                },
+                child: Text('組み合わせ'),
+              ),
+            ),
           ],
         ),
       ),
