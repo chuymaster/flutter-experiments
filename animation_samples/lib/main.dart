5bd98ef0d6697c182996d965c1cfd8f1b3d5b27e
Chuy <chuymaster3@hotmail.com>
2021-09-02 09:28:26 +0900

    Add TransitionPage

--- animation_samples/lib/main.dart
+++ animation_samples/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:animation_samples/hero_page.dart';
+import 'package:animation_samples/transition_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HeroFromPage(),
+      home: TransitionPage(),
     );
   }
 }
