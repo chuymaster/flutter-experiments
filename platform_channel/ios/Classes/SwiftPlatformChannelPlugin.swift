1b243474d5157937265659b63dec1dec345aa0b7
Chuy <chuymaster3@hotmail.com>
2021-09-09 09:57:26 +0900

    Add eventChannel code from the book (how can I write these codes without Xcode autocompletion?)

--- platform_channel/ios/Classes/SwiftPlatformChannelPlugin.swift
+++ platform_channel/ios/Classes/SwiftPlatformChannelPlugin.swift
@@ -10,6 +10,9 @@ public class SwiftPlatformChannelPlugin: NSObject, FlutterPlugin {
     let instance = SwiftPlatformChannelPlugin()
     instance.reachability = Reachability.forInternetConnection()
     registrar.addMethodCallDelegate(instance, channel: channel)
+
+    let eventChannel = FlutterEventChannel(name: "platform_channel/event_channel", binaryMessenger: registrar.messenger())
+    eventChannel.setStreamHandler(ReachabilityStreamHandler(reachability: instance.reachability))
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
@@ -21,4 +24,39 @@ public class SwiftPlatformChannelPlugin: NSObject, FlutterPlugin {
       result(FlutterMethodNotImplemented)
     }
   }
+}
+
+class ReachabilityStreamHandler: NSObject, FlutterStreamHandler {
+    let reachability: Reachability?
+    var eventSink: FlutterEventSink?
+
+    init(reachability: Reachability?) {
+        self.reachability = reachability
+    }
+
+    public func onListen(withArguments _: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        eventSink = events
+        // 初回の通知が来ないため、一度実行する
+        onNetworkStatusChange()
+        // ネットワークのステータスの変更通知イベントを受け取れる様に設定する
+        NotificationCenter.default.addObserver(self, selector: #selector(onNetworkStatusChange), name: NSNotification.Name.reachabilityChanged, object: nil)
+        reachability?.startNotifier()
+        return nil
+    }
+
+    public func onCancel(withArguments _: Any?) -> FlutterError? {
+        eventSink = nil
+        NotificationCenter.default.removeObserver(self)
+        reachability?.stopNotifier()
+        return nil
+    }
+
+    // ネットワークのステータス変更時にEventChannelのSinkへと流し込みます
+    @objc private func onNetworkStatusChange() {
+        guard let sink = eventSink else {
+            return
+        }
+        let stateValue = reachability?.currentReachabilityStatus().rawValue
+        sink(stateValue)
+    }
 }
