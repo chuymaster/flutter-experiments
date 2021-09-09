1ecc159abd144d9cd14c213c6a7bd922998d7749
Chuy <chuymaster3@hotmail.com>
2021-09-09 09:35:48 +0900

    Add platform channel

--- platform_channel/lib/platform_channel.dart
+++ platform_channel/lib/platform_channel.dart
@@ -1,11 +1,19 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class PlatformChannel {
-  static const MethodChannel _channel =
-      const MethodChannel('platform_channel');
+  static const MethodChannel _channel = const MethodChannel('platform_channel');
+  static const EventChannel _eventChannel =
+      EventChannel('platform_channel/event_channel');
+
+  static Future<bool> get isReachable async {
+    return await _channel.invokeMethod('isReachable');
+  }
+
+  static Stream<int> onNetworkStateChange() {
+    return _eventChannel.receiveBroadcastStream().map((event) => event as int);
+  }
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
