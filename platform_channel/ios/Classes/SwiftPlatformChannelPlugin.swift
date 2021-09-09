e74f61ab8c6744b480d69fffc503184d6b06a3b9
Chuy <chuymaster3@hotmail.com>
2021-09-09 09:48:19 +0900

    Implement Reachability to SwiftPlatformChannelPlugin

--- platform_channel/ios/Classes/SwiftPlatformChannelPlugin.swift
+++ platform_channel/ios/Classes/SwiftPlatformChannelPlugin.swift
@@ -1,14 +1,24 @@
 import Flutter
+import Reachability
 import UIKit
 
 public class SwiftPlatformChannelPlugin: NSObject, FlutterPlugin {
+  var reachability: Reachability?
+  
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "platform_channel", binaryMessenger: registrar.messenger())
     let instance = SwiftPlatformChannelPlugin()
+    instance.reachability = Reachability.forInternetConnection()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if call.method == "isReachable" {
+      result(reachability?.isReachable() ?? false)
+    } else if call.method == "getPlatformVersion" {
+      result("iOS " + UIDevice.current.systemVersion)
+    } else {
+      result(FlutterMethodNotImplemented)
+    }
   }
 }
